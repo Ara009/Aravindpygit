94cf4bc8e172f9c30f0b322e04799e4108c9ece5
aravi <aravindhrajendran@yahoo.com>
2024-04-09 16:13:18 +0530

    variables in python

---
+++ variables.py
@@ -0,0 +1,14 @@
+x = 2
+y = 2
+print(x+y)
+
+name = 'Youtube'
+print(name[0:2])  #fetch the value in the name
+print(name[-4]) #reverse order of the variable
+
+a = name + 'rock' #merging the variable
+print(a)
+
+myname='Aravind' #length of the variable
+print(len(myname))
+
